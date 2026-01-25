8b7918a3bbd965a2382080dbcab905a66d21d858
Tony Faijue <tonyf17191@gmail.com>
2026-01-24 18:51:37 -0600

    Added DTOs for NodeWeb

---
+++ api/SchemaStar/SchemaStar/DTOs/Nodeweb DTOs/NodewebResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Policy;
+
+namespace SchemaStar.DTOs.Nodeweb_DTOs
+{
+    //Expose Fields for Get Requests
+    public class NodewebResponseDTO
+    {
+        public Guid PublicId { get; set; } // Map to Guid from byte[]
+        public string NodeWebName { get; set; } = null!;
+        public DateTime? LastLayoutAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
