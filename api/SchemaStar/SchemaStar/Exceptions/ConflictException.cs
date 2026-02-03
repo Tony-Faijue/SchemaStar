6887f49a28258c2459d0c88cca6e187e7f61b396
Tony Faijue <tonyf17191@gmail.com>
2026-02-02 21:01:11 -0600

    Registered ProblemDetails Service & StatusCodePages Middleware & Custom Exceptions

---
+++ api/SchemaStar/SchemaStar/Exceptions/ConflictException.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace SchemaStar.Exceptions
+{
+    public sealed class ConflictException : AppException
+    {
+        public ConflictException(string message) 
+            : base(message, HttpStatusCode.Conflict)
+        {
+        }
+    }
+}
