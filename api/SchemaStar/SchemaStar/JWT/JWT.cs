8f9ec654d67f0f9daa46e6f4a73fc1c7db9b4a66
Tony Faijue <tonyf17191@gmail.com>
2026-02-06 17:10:30 -0600

    Added JWTBearer Package & JWT Class

---
+++ api/SchemaStar/SchemaStar/JWT/JWT.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchemaStar.JWT
+{
+    public class JWT
+    {
+
+        public string Key { get; set; } = null!;
+        public string Issuer { get; set; } = null!;
+        public string Audience { get; set; } = null!;
+        public double DurationInMinutes { get; set; } = 5.0;
+
+    }
+}
