ea485c4be353903f5bd065ae255d8f19ef4a0d79
Tony Faijue <tonyf17191@gmail.com>
2026-02-06 22:08:47 -0600

    Installed TailwindCSS & Added Initial Component Structure

--- web/SchemaStar/src/app/app.routes.ts
+++ web/SchemaStar/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { MainLayout } from './main-layout/main-layout';
+import { Welcome } from './pages/welcome/welcome';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayout, //Template Layout
+        children: [
+            {
+                //Child Components to be slotted in the mainlayout component
+                path: '',
+                component: Welcome
+            }
+        ]
+    },
+
+];
