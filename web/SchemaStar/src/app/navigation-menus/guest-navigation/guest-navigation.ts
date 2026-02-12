55ea7b25c185f2d532ec31d9ea3c1b16d7ff2b30
Tony Faijue <tonyf17191@gmail.com>
2026-02-11 18:52:53 -0600

    Added Guest Navigation Component

---
+++ web/SchemaStar/src/app/navigation-menus/guest-navigation/guest-navigation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-navigation',
+  imports: [],
+  templateUrl: './guest-navigation.html',
+  styleUrl: './guest-navigation.css',
+})
+export class GuestNavigation {
+
+}
