81c81283001795d9aa801555c71fd856b3c0f1c2
Tony Faijue <tonyf17191@gmail.com>
2026-02-13 19:33:36 -0600

    Added new exception classes & mapped them in GlobalExceptionHandler

---
+++ api/SchemaStar/SchemaStar/Exceptions/ContentTooLargeException.cs
@@ -0,0 +1,21 @@
+﻿namespace SchemaStar.Exceptions
+{
+    public sealed class ContentTooLargeException : AppException
+    {
+        public ContentTooLargeException(string message)
+            : base(message, System.Net.HttpStatusCode.RequestEntityTooLarge)
+        { 
+        }
+
+        public ContentTooLargeException(string resourceName, long? maxSizeBytes = null) 
+            : base(messageFormat(resourceName, maxSizeBytes), System.Net.HttpStatusCode.RequestEntityTooLarge)
+        { 
+        }
+
+        private static string messageFormat (string resourceName, long? maxSizeBytes)
+        {
+            var limitInfo = maxSizeBytes.HasValue ? $"(Limit: {maxSizeBytes / 1024 / 1024} MB)" : string.Empty;
+            return $"The request content for {resourceName} is too large and cannot be processed. {limitInfo}";
+        }
+    }
+}
