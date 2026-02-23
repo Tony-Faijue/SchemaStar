1c5b3c2e36b7d8f89fdd4f6e00584d2967085fee
Tony Faijue <tonyf17191@gmail.com>
2026-02-23 17:12:11 -0600

    Added Footer Component

--- web/SchemaStar/src/app/main-layout/main-layout.ts
+++ web/SchemaStar/src/app/main-layout/main-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { GuestNavigation } from '../navigation-menus/guest-navigation/guest-navigation';
+import { FooterMenu } from "../navigation-menus/footer-menu/footer-menu";
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterModule, GuestNavigation],
+  imports: [RouterModule, GuestNavigation, FooterMenu],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
