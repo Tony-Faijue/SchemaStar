34bc23d22535564abeeb389edf617dae71e3cd54
Tony Faijue <tonyf17191@gmail.com>
2026-03-04 22:11:03 -0600

    added user-menu-drawer

---
+++ web/SchemaStar/src/app/navigation-menus/user-menu/user-menu-drawer/user-menu-drawer.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-user-menu-drawer',
+  imports: [],
+  templateUrl: './user-menu-drawer.html',
+  styleUrl: './user-menu-drawer.css',
+})
+export class UserMenuDrawer {
+
+  @Input() isOpen = false; //state of the drawer from the parent
+  @Output() closeDrawer = new EventEmitter<void>(); //tell the parent to close the drawer
+
+  /**
+   * emit the closeDrawer event emitter to the parent component
+   */
+  close(){
+    this.closeDrawer.emit();
+  }
+}
--- web/SchemaStar/src/app/pages/dashboard/dashboard-layout/dashboard-layout.ts
+++ web/SchemaStar/src/app/pages/dashboard/dashboard-layout/dashboard-layout.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { DashboardSideMenu } from "../../../navigation-menus/dashboard-side-menu/dashboard-side-menu";
 import { UserMenu } from "../../../navigation-menus/user-menu/user-menu";
+import { UserMenuDrawer } from "../../../navigation-menus/user-menu/user-menu-drawer/user-menu-drawer";
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [DashboardSideMenu, UserMenu],
+  imports: [DashboardSideMenu, UserMenu, UserMenuDrawer],
   templateUrl: './dashboard-layout.html',
   styleUrl: './dashboard-layout.css',
 })
 export class DashboardLayout {
-
+  isUserMenuOpen: boolean = false;
 }
