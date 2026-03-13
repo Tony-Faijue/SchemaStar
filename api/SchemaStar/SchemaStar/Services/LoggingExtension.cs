71227fe89610407d6546dd63b45f4de882bf6f75
Tony Faijue <tonyf17191@gmail.com>
2026-03-13 17:05:39 -0500

    Added Serilog Logger for logging in .NET

---
+++ api/SchemaStar/SchemaStar/Services/LoggingExtension.cs
@@ -0,0 +1,15 @@
+﻿using Serilog;
+
+namespace SchemaStar.Services
+{
+    public static class LoggingExtension
+    {
+        public static void AddLoggingConfiguration(this WebApplicationBuilder builder) 
+        {
+            builder.Services.AddSerilog((services, lc) => lc
+                .ReadFrom.Configuration(builder.Configuration)
+                .ReadFrom.Services(services)
+            );
+        }
+    }
+}
