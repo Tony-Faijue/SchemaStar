e777f5d4459aa3fbde9c4e77be3afaeb4b42a986
Tony Faijue <tonyf17191@gmail.com>
2026-04-06 15:24:12 -0500

    Migration: FixNodeWebNodeRelationship & Modified Node DTOs

--- api/SchemaStar/SchemaStar/DTOs/Node DTOs/NodeRequestDTO.cs
+++ api/SchemaStar/SchemaStar/DTOs/Node DTOs/NodeRequestDTO.cs
@@ -10,41 +10,22 @@ namespace SchemaStar.DTOs.Node_DTOs
 
         [StringLength(16383)]
         public string? NodeDescription {get; set;}
-
-        [StringLength(512)]
-        public string? NodeImageUrl { get; set;}
-
-        [StringLength(512)]
-        public string? NodeAudioUrl { get; set;}
-
-        [StringLength(64)]
-        [RegularExpression(@"^image/(jpeg|png|gif|bmp|webp)$", ErrorMessage = "Invalid Image MIME type")]
-        public string? NodeImageMime { get; set;}
         
-        [StringLength(64)]
-        [RegularExpression(@"^audio/(mpeg|wav|ogg|mp4|x-m4a)$", ErrorMessage ="Invalid Audio MIME type")]
-        public string? NodeAudioMime { get; set; }
+        public double PositionX { get; set;}
 
-        [Range(1, 20971520, ErrorMessage ="Image size cannot be greater than 20MB and must contain at least 1 byte")]
-        public int? NodeImageSize { get; set;}
-
-        [Range(1, 52428800, ErrorMessage = "Audio size cannot be greater than 50MB and must contain at least 1 byte")]
-        public int? NodeAudioSize { get; set;}
-
-        [Range(-50000, 50000, ErrorMessage = "Node is too far from center")]
-        public double? PositionX { get; set;}
-
-        [Range(-50000, 50000, ErrorMessage = "Node is too far from center")]
-        public double? PositionY { get; set;}
+        public double PositionY { get; set;}
 
         [Required]
-        [Range(120, 800, ErrorMessage = "Width size should be between 120 - 800")]
+        [Range(120, 2000, ErrorMessage = "Width size should be between 120 - 2000")]
         public int Width { get; set; } = 200;
 
         [Required]
-        [Range(80, 1200, ErrorMessage = "Height size should be between 80 - 1200")]
+        [Range(80, 5000, ErrorMessage = "Height size should be between 80 - 5000")]
         public int Height { get; set; } = 152;
 
-        public NodeState? State { get; set;}
+        public NodeState State { get; set; } = NodeState.Unlocked;
+
+        [Required]
+        public Guid NodeWebId { get; set; }
     }
 }
--- api/SchemaStar/SchemaStar/DTOs/Node DTOs/NodeResponseDTO.cs
+++ api/SchemaStar/SchemaStar/DTOs/Node DTOs/NodeResponseDTO.cs
@@ -4,19 +4,18 @@ namespace SchemaStar.DTOs.Node_DTOs
 {
     public class NodeResponseDTO
     {
-        public Guid publicId { get; set; } //Map to Guid from byte []
+        public Guid PublicId { get; set; } //Map to Guid from byte []
         public string NodeName { get; set; } = null!;
         public string? NodeDescription { get; set; }
-        public string? NodeImageUrl { get; set; }
-        public string? NodeAudioUrl { get; set; }
-        public string? NodeImageMime { get; set; }
-        public string? NodeAudioMime { get; set; }
-        public int? NodeImageSize { get; set; }
-        public int? NodeAudioSize { get; set; }
-        public double? PositionX { get; set; }
-        public double? PositionY { get; set; }
+        public double PositionX { get; set; }
+        public double PositionY { get; set; }
         public int Width { get; set; }
         public int Height { get; set; }
-        public NodeState? State { get; set; }
+        public NodeState State { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public Guid NodeWebId { get; set; }
     }
 }
