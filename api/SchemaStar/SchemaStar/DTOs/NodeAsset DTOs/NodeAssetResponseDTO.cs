9d25f94063439a39e4401210df964452d99739bb
Tony Faijue <tonyf17191@gmail.com>
2026-04-06 16:49:22 -0500

    Migration: ModifiedNodeAssetWithBlobNameAndNodeAssetName

---
+++ api/SchemaStar/SchemaStar/DTOs/NodeAsset DTOs/NodeAssetResponseDTO.cs
@@ -0,0 +1,17 @@
+﻿using SchemaStar.Models.Enums;
+
+namespace SchemaStar.DTOs.NodeAsset_DTOs
+{
+    public class NodeAssetResponseDTO
+    {
+        public Guid PublicId { get; set; }
+        public string? NodeAssetName { get; set; }
+        public NodeAssetEnums.NodeAssetType NodeAssetType { get; set; }
+        public NodeAssetEnums.NodeAssetSource NodeAssetSource { get; set; }
+        public string? Url { get; set; }
+        public string? MimeType { get; set; }
+        public int? FileSize { get; set; }
+        public string? BlobPath { get; set; }
+        public Guid NodeId { get; set; }
+    }
+}
--- api/SchemaStar/SchemaStar/Models/NodeAsset.cs
+++ api/SchemaStar/SchemaStar/Models/NodeAsset.cs
@@ -16,6 +16,10 @@ namespace SchemaStar.Models
         [MaxLength(16)]
         public byte[] PublicId { get; set; } = null!;
 
+        [Column("node_asset_name")]
+        [StringLength(255)]
+        public string? NodeAssetName { get; set; }
+
         [Required]
         [Column("asset_type")]
         public NodeAssetEnums.NodeAssetType NodeAssetType { get; set; }
@@ -30,17 +34,20 @@ namespace SchemaStar.Models
 
         [Column("mime_type")]
         [StringLength(127)]
-        public string MimeType { get; set; } = null!;
+        public string? MimeType { get; set; }
 
         [Column("file_size")]
         [Range(1, 104857600, ErrorMessage = "File size cannot be greater than 100MB and must contain at least 1 byte")]
         public int? FileSize { get; set; }
 
+        [Column("blob_path")]
+        [StringLength(1024)]
+        public string? BlobPath { get; set; } //the file path for the blob resource
+
         [Required]
         [Column("node_id")]
         public ulong NodeId { get; set; }
 
-        [ForeignKey("NodeId")]
         public virtual Node Node { get; set; } = null!;
 
     }
