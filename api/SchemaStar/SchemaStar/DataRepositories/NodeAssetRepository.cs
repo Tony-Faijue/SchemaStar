66fbc329bd3d8993e64955ac534a3c659549b198
Tony Faijue <tonyf17191@gmail.com>
2026-04-07 22:12:40 -0500

    Data Repositories for NodeAsset/Edge; Unit Tests GetNodewebFull()

---
+++ api/SchemaStar/SchemaStar/DataRepositories/NodeAssetRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchemaStar.Models;
+
+namespace SchemaStar.DataRepositories
+{
+    public class NodeAssetRepository : INodeAssetRepository
+    {
+        private readonly SchemastarContext _context;
+
+        public NodeAssetRepository(SchemastarContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Gets a list of node assets
+        /// </summary>
+        /// <param name="nodePublicId"></param>
+        /// <param name="userId"></param>
+        /// <returns> a list of NodeAssets that belong the node and use</returns>
+        public async Task<IEnumerable<NodeAsset>> GetNodeAssetsByNodeIdAsync(byte[] nodePublicId, ulong userId)
+        {
+            return await _context.NodeAssets
+                .Where(n => n.Node.PublicId == nodePublicId && n.Node.NodeWeb.UserId == userId)
+                .ToListAsync();
+        }
+
+        /// <summary>
+        /// Gets the specific node asset
+        /// </summary>
+        /// <param name="publicId"></param>
+        /// <param name="userId"></param>
+        /// <returns>a node asset with the given public id</returns>
+        public async Task<NodeAsset?> GetNodeAssetByPublicIdAsync(byte[] publicId, ulong userId)
+        {
+            return await _context.NodeAssets
+                .FirstOrDefaultAsync(n => n.PublicId == publicId && n.Node.NodeWeb.UserId == userId);
+        }
+
+        public void Add(NodeAsset nodeAsset) => _context.NodeAssets.Add(nodeAsset);
+
+        public void Delete(NodeAsset nodeAsset) => _context.NodeAssets.Remove(nodeAsset);
+
+        public Task SaveChangesAsync() => _context.SaveChangesAsync();
+
+    }
+}
