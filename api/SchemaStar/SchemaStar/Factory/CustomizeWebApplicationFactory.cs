71252065d6c125cdd1ad5fa5076957a7b7054434
Tony Faijue <tonyf17191@gmail.com>
2026-04-30 23:11:37 -0500

    Integration Test Project and Factory Setup along with Version Updates

---
+++ api/SchemaStar/SchemaStar/Factory/CustomizeWebApplicationFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace SchemaStar.Factory
+{
+    //public class CustomizeWebApplicationFactory<TProgram> 
+    //    : WebApplicationFactory<TProgram> where TProgram : class
+    //{
+    //}
+}
